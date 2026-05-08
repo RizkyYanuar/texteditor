992e4203a0a4129bcd79aad03be1f2fd7ff09fd5
Hafiez Novrizal putra <hafieznp611@gmail.com>
2026-05-08 14:10:39 +0700

    Memperbaiki bug difitur baris baru dimana baris barunya tidak muncul + perbaikan logika-logika ketika enter di awal baris, di awal kolom, dan di akhir baris (belum bisa di tengah baris memotong teks), Menambahkan fitur pindah kursor antar baris, Mengubah konsep koordinat kursor, menambah konsep linkedlist untuk baris yang tadinya hanya punya 3 subvar yaitu prev,kol,next menjadi prev,kol,next,longBar yaitu untuk menyimpan panjang baris tersebut, memperbaiki tampilan di CLI

--- coba coba konsep linked list Dobli.cpp
+++ coba coba konsep linked list Dobli.cpp
@@ -1,3 +1,4 @@
+//===========================================================================================Head=======================================================================================
 #include <stdio.h>
 #include <stdlib.h>
 #include <conio.h>
@@ -17,154 +18,305 @@ typedef struct Kolom {
 } Kol;
 
 typedef struct Baris {
-	addressBar prev; 
+    addressBar prev;
     addressKol kol;
-    addressBar next; 
+    addressBar next;
+    int longBar;
 } Bar;
+//===========================================================================================NECK=======================================================================================
+// ================= CURSOR =================
+void setCursor(int x, int y) {
+    COORD coord;
+    coord.X = x;
+    coord.Y = y;
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
+}
 
-void setCursor(int x, int y);
-void printBar(Bar *bar,Bar *FB);
-int getCursorIndex(Bar *bar, addressKol Cursor);
+// ================= PRINT =================
+void printBar(addressBar FirstBar) {
+	int i = 1;
+    while (FirstBar != Nil) {
+        addressKol Q = FirstBar->kol;
+		printf("%d| ",i);
+        while (Q != Nil) {
+            printf("%c", Q->info);
+            Q = Q->next;
+        }
+
+//        printf("   | longBar = %d", FirstBar->longBar);
+        printf("\n");
+        FirstBar = FirstBar->next;
+        i++;
+    }
+}
+
+
+void freeAll(addressBar FirstBar) {
+    while (FirstBar != Nil) {
+        addressKol Q = FirstBar->kol;
+
+        while (Q != Nil) {
+            addressKol tempK = Q;
+            Q = Q->next;
+            free(tempK);
+        }
+
+        addressBar tempB = FirstBar;
+        FirstBar = FirstBar->next;
+        free(tempB);
+    }
+}
+
+int PCX(int CurX, int perubahan){
+    return CurX + perubahan;
+}
+
+int PCY(int CurY, int perubahan){
+    return CurY + perubahan;
+}
+
+
+//===========================================================================================Body=======================================================================================
+
 
 int main() {
-    Bar *bar = Nil, *CurrentBar, *FirstBar;
+    addressBar FirstBar = Nil;
+    addressBar CurrentBar = Nil;
     addressKol Cursor = Nil;
     addressKol Q;
+    int CursorX = 3,CursorY = 1;
 
     int key;
-	printf(" TEXT EDITOR COBA COBA \n");
-	
-    while (1) {
-        key = getch();
-		
-		if (key ==27 ){
-			if (key == 27) {
-   				Bar *B = FirstBar;
 
-	    		while (B != Nil) {
-	        		addressKol Q = B->kol;
-	
-	        		while (Q != Nil) {
-	            		addressKol tempK = Q;
-	            		Q = Q->next;
-	            		free(tempK);
-	        		}
-	
-					Bar *tempB = B;
-	        		B = B->next;
-	        		free(tempB);
-    			}
-			} 	
-			printf("\n\nMemory Berhasil Di Bebaskan, Gacor Kang");	
-			break;
-		}
+    printf(" TEXT EDITOR COBA COBA \n");
+
+    while (1) {
+    	
+    	
+        key = getch();
+
+
+        if (key == 27) {
+            freeAll(FirstBar);
+            printf("\n\n\n================ Memory dibebaskan ======================\n\n\n");
+            break;
+        }
+
+
         if (key == 224 || key == 0) {
             key = getch();
 
-            // kiri
             if (key == 75) {
-                if (Cursor != Nil)
-                    Cursor = Cursor->prev;
-                    
+			    if (Cursor != Nil){
+			        Cursor = Cursor->prev; //Ketika disebah kiri ada karakter alias ada node dia Cursor akan geser terus ke awal node(BackEnd)
+			    }
+			    if (CursorX > 3)
+    				CursorX = PCX(CursorX, -1); //Selama dia ga berada di awal baris maka cursor akan bergerak ke kiri(FrontEnd) kalau sudah di ujung ya dia ga ngapa ngapain
+			}
+
+            else if (key == 77) {
+			    if (Cursor == Nil && CurrentBar != Nil){   //Ketika Kursor berada di awal baris alias tidak menunjuk node maka akan menunjuk node pertama di baris tersebut
+			        Cursor = CurrentBar->kol;                  
+			    }
+			    else if (Cursor != Nil && Cursor->next != Nil){     //Selama Kursor sedang menunjuk node maka Cursor akan menunjuk node setelahnya (backEnd) 
+			        Cursor = Cursor->next;						
+			    }
+			    if (CursorX - 3 < CurrentBar->longBar) //Selama posisi Kursor tidak melebihi panjang suatu abris maka dia bisa bergeser ke kanan
+    				CursorX = PCX(CursorX, + 1);			   
+			}
+			
+			
+			else if (key == 72){
+				if(CurrentBar->prev == Nil) //jika diatasnya tidak ada baris maka tidak melakukan apa apa
+				continue;
+				
+				if (CurrentBar->longBar <= CurrentBar->prev->longBar || CursorX - 3 <= CurrentBar->prev->longBar){   //ketika baris saat ini lebih pendek daro baris di atasnya maka posisi kursor di kolom tidak pindah hanya posisi abrisnya saja
+					CurrentBar = CurrentBar->prev; 
+					
+					if(Cursor == Nil){ // kalau posisi kursor ada dia wal baris alias nul maka ketika pindah baris juga cursor akan Nil
+						Cursor = Nil;
+					} 
+					else{
+						Cursor = CurrentBar->kol;
+						for (int i = 1; i < CursorX - 3; i++){         //maka kursor akan digeser sehauh posisi kursor terakhir
+							Cursor = Cursor->next;
+						}
+					}
+				}
+				
+				
+				else {                                                     //Ketika saat ini lebih panjang dari baris diatasnya maka posisi kursor akan berada di ujung baris diatasnya dan poisisi kursor akan pindah ke abris dia tasnya
+					CurrentBar = CurrentBar->prev; 
+					Cursor = CurrentBar->kol;
+					for (int i = 1; i < CurrentBar->longBar ; i++){
+						Cursor = Cursor->next;	 							//maka kursor akan digeser sehauh panjang baris
+					}
+					CursorX = CurrentBar->longBar + 3;
+				}
+				CursorY = PCY(CursorY,-1);
+			}
+			
+			else if (key == 80){
+				if (CurrentBar->next == Nil)
+					continue;
+					
+				if (CurrentBar->longBar <= CurrentBar->next->longBar || CursorX - 3 <= CurrentBar->next->longBar){   //ketika baris saat ini lebih pendek dari baris dibawhnay maka posisi kursor dikolom tidak akan berubahn posisi barisnya saja yang berubah
+					CurrentBar = CurrentBar->next; 
+					if(Cursor == Nil){               // kalau cursor sekarang ada di awal baris atau = Nil maka ketika pindah ke bawah dia juga bernilai Nil Cursornya
+						Cursor = Nil;
+					}
+					
+					else{
+						Cursor = CurrentBar->kol;
+						for (int i = 1; i < CursorX - 3 ; i++){             
+							Cursor = Cursor->next;     //Cursor digeser sebanyak posisi kursor terakhir
+						}
+					}
+				}
+				else {                                                     //ketika baris di bawahnya lebih pendek dari baris saat ini maka posisi kursor akan berada di ujung baris di bawahnya dan pindah ke abris dibawhnya
+					CurrentBar = CurrentBar->next; 
+					Cursor = CurrentBar->kol;
+					for (int i = 1 ;i < CurrentBar->longBar ; i++){
+						Cursor = Cursor->next;
+					}
+					CursorX = CurrentBar->longBar + 3;
+				}
+				CursorY = PCY(CursorY,+1);
+			}
+		
+            system("cls");
+            printf(" TEXT EDITOR COBA COBA \n");
+            printBar(FirstBar);
+            setCursor(CursorX, CursorY);
+            continue;
+        }
+
+
+        if (key == 8) { // Backspace
+            if (Cursor == Nil) continue;
+
+            addressKol temp = Cursor;
+
+            if (Cursor->prev != Nil) {
+                Cursor->prev->next = Cursor->next;
+
+                if (Cursor->next != Nil)
+                    Cursor->next->prev = Cursor->prev;
+
+                Cursor = Cursor->prev;
+            }
+            else {
+                if (Cursor->next != Nil) {
+                    CurrentBar->kol = Cursor->next;
+                    Cursor = Cursor->next;
+                    Cursor->prev = Nil;
+                    Cursor = Nil;
+                }
+                else {
+                    Cursor = Nil;
+                    CurrentBar->kol = Nil;
+                }
             }
 
-            // kanan
-            else if (key == 77) {
-                if (Cursor == Nil) {
-                    if (bar != Nil)
-                        Cursor = bar->kol;
-                }
-                else if (Cursor->next != Nil) {
-                    Cursor = Cursor->next;
-                }
-            }
+            CurrentBar->longBar--;
+            if (CursorX > 0)
+    			CursorX = PCX(CursorX, -1);
+            free(temp);
 
             system("cls");
             printf(" TEXT EDITOR COBA COBA \n");
-            printBar(bar,FirstBar);
-
-            int pos = getCursorIndex(bar, Cursor);
-            setCursor(pos, 1);
+            printBar(FirstBar);
+            setCursor(CursorX, CursorY);
             continue;
         }
-        
-        if (key == 8){
-    		if (Cursor == Nil){
-    			continue;
+
+
+        if (key == 13) { // Enter baris baru
+
+            addressBar newBar = (addressBar) malloc(sizeof(Bar));
+            newBar->kol = Nil;
+            newBar->next = Nil;
+            newBar->prev = Nil;
+            newBar->longBar = 0;
+            CursorX = 3;
+            
+
+            if (CurrentBar == Nil) {
+                FirstBar = newBar;
+                CurrentBar = newBar;
+            }
+            
+            else if (CurrentBar->prev == Nil && Cursor == Nil) {
+
+                newBar->next = CurrentBar;
+                newBar->prev = Nil;
+
+                CurrentBar->prev = newBar;
+
+                FirstBar = newBar;
+                CurrentBar = newBar;
+                CursorY = PCY(CursorY,0);
+            }
+            
+            else if(CurrentBar->prev != Nil && Cursor == Nil){
+            	CurrentBar->prev->next = newBar;
+            	newBar->prev = CurrentBar->prev;
+            	newBar->next = CurrentBar;
+            	CurrentBar->prev = newBar;
+            	CurrentBar = newBar;
+            	CursorY = PCY(CursorY,0);
+            	
 			}
-        	
-   			addressKol temp = Cursor;
+			
+            else {
+                newBar->next = CurrentBar->next;
+                newBar->prev = CurrentBar;
 
+                if (CurrentBar->next != Nil)
+                    CurrentBar->next->prev = newBar;
 
-    		if (Cursor->prev != Nil){
-        		Cursor->prev->next = Cursor->next;
+                CurrentBar->next = newBar;
+                CurrentBar = newBar;
+                CursorY = PCY(CursorY,1);
+            }
+	
+            Cursor = Nil;
 
-        		if(Cursor->next != Nil){
-           			Cursor->next->prev = Cursor->prev; 
-        		}
-        		Cursor = Cursor->prev;
-    		}
+            system("cls");
+            printf(" TEXT EDITOR COBA COBA \n");
+            printBar(FirstBar);
+            
+            setCursor(CursorX, CursorY);
+            continue;
+        }
 
-    		else { 
-        		if(Cursor->next != Nil){
-            		bar->kol = Cursor->next; //ini kalo misalnya di awal tapi disebelahkanannya ada karakter == A|BC == maka jadi == |BC == dan kursor harus dibikin nil 
-            		Cursor = Cursor->next;
-            		Cursor->prev = Nil;
-            		Cursor = Nil;
-       			 }
-        		else{
-            		Cursor = Nil;
-            		bar->kol = Nil; 
-       				}	
-    		}
-
-    		free(temp);
-
-		    system("cls");
-		    printf(" TEXT EDITOR COBA COBA \n");
-		    printBar(bar,FirstBar);
-		
-		    int pos = getCursorIndex(bar, Cursor);
-		    setCursor(pos, 1);
-
-    		continue;
-		}
-		
-		
-		if (key == 13){
-			bar = (Bar*) malloc(sizeof(Bar));
-			CurrentBar->next = bar;
-			CurrentBar->next->prev = CurrentBar; // harus di kembangin lagi euy soalnya banyak yang pemisalan kaya misal kalo CurrentBar->kol = Nil nanti dia ngehapus Baris dll
-			bar->next = Nil;
-		}
-		
-
+        //Insert karakter
         Q = (addressKol) malloc(sizeof(Kol));
         Q->info = key;
         Q->prev = Nil;
         Q->next = Nil;
 
-        // belum ada baris sama sekali
-        if (bar == Nil) {
-            bar = (Bar*) malloc(sizeof(Bar));
-            bar->kol = Q;
+        if (CurrentBar == Nil) {
+            addressBar newBar = (addressBar) malloc(sizeof(Bar));
+            newBar->kol = Q;
+            newBar->prev = Nil;
+            newBar->next = Nil;
+            newBar->longBar = 0;
+
+            FirstBar = newBar;
+            CurrentBar = newBar;
             Cursor = Q;
-            CurrentBar = bar;
-            FirstBar = bar;
         }
         else {
-            // insert di awal
             if (Cursor == Nil) {
-                Q->next = bar->kol;
+                Q->next = CurrentBar->kol;
 
-                if (bar->kol != Nil)
-                    bar->kol->prev = Q;
+                if (CurrentBar->kol != Nil)
+                    CurrentBar->kol->prev = Q;
 
-                bar->kol = Q;
+                CurrentBar->kol = Q;
                 Cursor = Q;
             }
-            
-            //insert ditengah atau di ujung tergantung Cursor->next ada atau engga riweh pokonamah
             else {
                 Q->next = Cursor->next;
                 Q->prev = Cursor;
@@ -177,62 +329,14 @@ int main() {
             }
         }
 
+        CurrentBar->longBar++;
+		CursorX = PCX(CursorX, 1);
+		
         system("cls");
         printf(" TEXT EDITOR COBA COBA \n");
-        printBar(bar,FirstBar);
-
-        int pos = getCursorIndex(bar, Cursor);
-        setCursor(pos, 1);
+        printBar(FirstBar);
+        setCursor(CursorX, CursorY);
     }
 
     return 0;
-}
-
-
-void setCursor(int x, int y) {
-    COORD coord;
-    coord.X = x;
-    coord.Y = y;
-    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
-}
-
-void printBar(Bar *bar, Bar *FB) {
-    if (FB == Nil) {
-        printf("\n");
-        return;
-    }
-
-    while (FB != Nil) {
-        addressKol Q = FB->kol;  
-
-        while (Q != Nil) {
-            printf("%c", Q->info); //ini juga aku udah nyoba nyoba tapi masih ga jalan euy
-            Q = Q->next;
-        }
-
-        printf("\n");
-
-        FB = FB->next;  
-    }
-}
-
-int getCursorIndex(Bar *bar, addressKol Cursor) {
-    int index = 0;
-
-    if (bar == Nil)
-        return 0;
-
-    if (Cursor == Nil)
-        return 0;
-
-    addressKol Q = bar->kol;
-
-    while (Q != Nil) {
-        index++;
-        if (Q == Cursor)
-            return index;
-        Q = Q->next;
-    }
-
-    return index;
 }

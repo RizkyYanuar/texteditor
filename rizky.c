5a0eb02e3a1456c9e7c102fcb8826ef0c65ebb9b
hafiez2 <hafieznp611@gmail.com>
2026-05-26 16:12:25 +0700

    fix bug fitur enter dll

--- rizky.c
+++ rizky.c
@@ -6,7 +6,7 @@ void printBar(addressBar FirstBar) {
 	addressBar temp = FirstBar;
     while (temp != Nil) {
         addressKol Q = temp->kol;
-		printf("%d| ",i);
+		printf("  %-2d|",i);
         while (Q != Nil) {
             printf("%c", Q->info);
             Q = Q->next;

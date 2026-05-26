3dd8d1823b6c0ea9e8e2228c31f9318c49e41f34
NajwanRachman <najwanrachman8@gmail.com>
2026-05-26 16:27:59 +0700

    oke aman , update tampilan awal

--- najwan.c
+++ najwan.c
@@ -1,21 +1,21 @@
 #include "najwan.h"
 
 
-//void freeAll(addressBar FirstBar) {
-//    while (FirstBar != Nil) {
-//        addressKol Q = FirstBar->kol;
-//
-//        while (Q != Nil) {
-//            addressKol tempK = Q;
-//            Q = Q->next;
-//            free(tempK);
-//        }
-//
-//        addressBar tempB = FirstBar;
-//        FirstBar = FirstBar->next;
-//        free(tempB);
-//    }
-//}
+void freeAll(addressBar FirstBar) {
+    while (FirstBar != Nil) {
+        addressKol Q = FirstBar->kol;
+
+        while (Q != Nil) {
+            addressKol tempK = Q;
+            Q = Q->next;
+            free(tempK);
+        }
+
+        addressBar tempB = FirstBar;
+        FirstBar = FirstBar->next;
+        free(tempB);
+    }
+}
 
 
 void SaveFile(char *filename,addressBar FirstBar){
